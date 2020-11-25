1a4927dd123e7be6a155f7929033f8711d42797d
shina1 <oswasiu@gmail.com>
2020-11-25 11:26:58 +0200

    testing ci build

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('ci with travis');
+  res.send('ci with travis is so cool');
 });
 
 const server = app.listen(8080, () => {
